261fd310a4002fe2376e5e4de71f76bea3365320
christoferson <sweetsorrow1981@yahoo.co.jp>
2023-10-17 11:02:15 +0900

    openai connection sample

---
+++ main.py
@@ -0,0 +1,19 @@
+import os
+import requests
+import json
+import openai
+import config
+
+openai.api_key = config.azure_dev["openai_key"] #os.getenv("AZURE_OPENAI_KEY")
+openai.api_base = config.azure_dev["endpoint"] #os.getenv("AZURE_OPENAI_ENDPOINT") 
+openai.api_type = 'azure'
+openai.api_version = '2023-05-15' # this may change in the future
+
+deployment_name = config.azure_dev["deployment_name"] #This will correspond to the custom name you chose for your deployment when you deployed a model. 
+
+# Send a completion call to generate an answer
+print('Sending a test completion job')
+start_phrase = 'Write a tagline for an ice cream shop. '
+response = openai.Completion.create(engine=deployment_name, prompt=start_phrase, max_tokens=50)
+text = response['choices'][0]['text'].replace('\n', ' ').replace(' .', '.').strip()
+print(start_phrase + " : " + text)
