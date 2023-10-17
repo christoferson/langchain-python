189c1b194af90e9fcf977e9920ebcf316179352b
christoferson <sweetsorrow1981@yahoo.co.jp>
2023-10-17 17:09:08 +0900

    Add demo_llm_generate

--- demo_bedrock_langchain.py
+++ demo_bedrock_langchain.py
@@ -12,8 +12,11 @@ def run_demo(session):
 
     model_kwargs = { "temperature": 0.0 }
     prompt = "Tell me a fun fact about Pluto."
+    prompts = ["Tell me a fun fact about Pluto.", "Tell me a fun fact about Venus."]
 
-    demo_llm(bedrock_runtime, model_id, model_kwargs, prompt)
+    #demo_llm(bedrock_runtime, model_id, model_kwargs, prompt)
+    #demo_llm_predict(bedrock_runtime, model_id, model_kwargs, prompt)
+    demo_llm_generate(bedrock_runtime, model_id, model_kwargs, prompts)
 
 
 def demo_llm(bedrock_runtime, model_id, model_kwargs, prompt):
@@ -26,6 +29,41 @@ def demo_llm(bedrock_runtime, model_id, model_kwargs, prompt):
         model_kwargs = model_kwargs,
     )
 
-    output = llm(prompt) #output = llm.predict(prompt)
+    output = llm(prompt)
 
     print(output)
+
+def demo_llm_predict(bedrock_runtime, model_id, model_kwargs, prompt):
+
+    print("Call demo_llm_predict")
+
+    llm = Bedrock(
+        client = bedrock_runtime,
+        model_id = model_id,
+        model_kwargs = model_kwargs,
+    )
+
+    output = llm.predict(prompt)
+
+    print(output)
+
+def demo_llm_generate(bedrock_runtime, model_id, model_kwargs, prompts):
+
+    print("Call demo_llm_generate")
+
+    llm = Bedrock(
+        client = bedrock_runtime,
+        model_id = model_id,
+        model_kwargs = model_kwargs,
+    )
+
+    output = llm.generate(prompts)
+
+    #print(output.schema())
+    print(output.llm_output)
+
+    for generation in output.generations:
+        print(generation[0].text)
+        print("")
+
+    
