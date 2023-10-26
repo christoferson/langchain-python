e56d18e1d4af2ef41a308b3a041ed4da921781ce
christoferson <sweetsorrow1981@yahoo.co.jp>
2023-10-26 21:01:30 +0900

    Add demo_vectordb_multiquery_retriever

--- demo_bedrock_langchain_multiquery.py
+++ demo_bedrock_langchain_multiquery.py
@@ -1,3 +1,5 @@
+import logging
+
 from langchain.document_loaders import CSVLoader
 from langchain.document_loaders import BSHTMLLoader
 from langchain.document_loaders import PyPDFLoader
@@ -13,6 +15,9 @@ from langchain.embeddings import BedrockEmbeddings
 
 from langchain.vectorstores import Chroma
 
+from langchain.retrievers.multi_query import MultiQueryRetriever
+
+from langchain.prompts import PromptTemplate
 
 CHROMA_DB_PATH = "vectordb/chromadb/demo.db"
 
@@ -27,13 +32,23 @@ def run_demo(session):
     model_id = "anthropic.claude-instant-v1"
     model_id = "amazon.titan-embed-text-v1"
 
+    model_kwargs = { "temperature": 0.0 }
+#['Here are 3 alternative questions for "What is the origin of the name New York?":', '', 'What was the original name of the city now known as New York and how did it get its current name?', '', 'What is the etymology and meaning behind the name of the city of New York? ', '', 
+#'What was the place that is now called New York originally called by native inhabitants or early European settlers, and when/why did it start being referred to as New 
+#York?']
     #demo_load_embed_save(bedrock_runtime)
-    prompt = "What is the origin of the name New York?"
-    prompt = "Etymology New York"
+    prompt = "What is the meaning behind the name New York?"
+    #prompt = "Etymology New York"
+
+    setup_loggers()
+
     #demo_vectordb_similarity_search(bedrock_runtime, model_id, prompt)
-    demo_vectordb_retriever(bedrock_runtime, model_id, prompt)
+    #demo_vectordb_retriever(bedrock_runtime, model_id, prompt)
+    demo_vectordb_multiquery_retriever(bedrock_runtime, "anthropic.claude-instant-v1", model_kwargs, prompt)
 
-
+def setup_loggers():
+    logging.basicConfig()
+    logging.getLogger('langchain.retrievers.multi_query').setLevel(logging.INFO)
 
 def demo_load_embed_save(bedrock_runtime : str, model_id='amazon.titan-embed-text-v1'):
 
@@ -94,6 +109,46 @@ def demo_vectordb_retriever(bedrock_runtime : str, model_id, prompt):
     retriever = vectordb.as_retriever()
 
     similar_docs = retriever.get_relevant_documents(prompt, kwargs={ "k": 2 })
+
+    print(f"Matches: {len(similar_docs)}")
+    for similar_doc in similar_docs:
+        print("---------------------------------")
+        print(f"{similar_doc.metadata}")
+        print(f"{similar_doc.page_content}")
+
+PROMPT = PromptTemplate(
+    input_variables=["question"],
+    template="""You are an AI language model assistant. Your task is 
+    to generate 3 different versions of the given user 
+    question to retrieve relevant documents from a vector  database. 
+    By generating multiple perspectives on the user question, 
+    your goal is to help the user overcome some of the limitations 
+    of distance-based similarity search. Provide these alternative 
+    questions separated by newlines. Original question: {question}""",
+)
+
+def demo_vectordb_multiquery_retriever(bedrock_runtime : str, model_id, model_kwargs, prompt):
+
+    print("Call demo_vectordb_multiquery_retriever")
+
+    embeddings = BedrockEmbeddings(
+        client = bedrock_runtime,
+        model_id = "amazon.titan-embed-text-v1"
+    )
+
+    llm = BedrockChat(
+        client = bedrock_runtime,
+        model_id = "anthropic.claude-instant-v1",
+        model_kwargs = model_kwargs,
+    )
+
+    vectordb = Chroma(embedding_function=embeddings, persist_directory=CHROMA_DB_PATH)
+
+    retriever = vectordb.as_retriever()
+
+    multiquery_retriever = MultiQueryRetriever.from_llm(retriever = retriever, llm = llm)
+
+    similar_docs = multiquery_retriever.get_relevant_documents(prompt)
 
     print(f"Matches: {len(similar_docs)}")
     for similar_doc in similar_docs:
