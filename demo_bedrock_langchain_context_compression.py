124d61841d5c7ec782b856f49477a834f2ba5cef
christoferson <sweetsorrow1981@yahoo.co.jp>
2023-10-28 01:05:08 +0900

    demo_vectordb_contextual_retriever

--- demo_bedrock_langchain_context_compression.py
+++ demo_bedrock_langchain_context_compression.py
@@ -16,6 +16,8 @@ from langchain.embeddings import BedrockEmbeddings
 from langchain.vectorstores import Chroma
 
 from langchain.retrievers.multi_query import MultiQueryRetriever
+from langchain.retrievers import ContextualCompressionRetriever
+from langchain.retrievers.document_compressors import LLMChainExtractor
 from pydantic import BaseModel, Field
 from langchain.output_parsers import PydanticOutputParser
 
@@ -47,7 +49,8 @@ def run_demo(session):
     #demo_vectordb_similarity_search(bedrock_runtime, model_id, prompt)
     #demo_vectordb_retriever(bedrock_runtime, model_id, prompt)
     #demo_vectordb_multiquery_retriever(bedrock_runtime, "anthropic.claude-instant-v1", model_kwargs, prompt)
-    demo_vectordb_multiquery_retriever_m2(bedrock_runtime, prompt = prompt)
+    #demo_vectordb_multiquery_retriever_m2(bedrock_runtime, prompt = prompt)
+    demo_vectordb_contextual_retriever(bedrock_runtime, prompt = prompt)
 
 
 def setup_loggers():
@@ -188,7 +191,8 @@ QUERY_PROMPT = PromptTemplate(
 def demo_vectordb_multiquery_retriever_m2(bedrock_runtime : str, 
                                           embedding_model_id = "amazon.titan-embed-text-v1", 
                                           llm_model_id = "anthropic.claude-instant-v1", 
-                                          llm_model_kwargs = { "temperature": 0.0 }, prompt = None):
+                                          llm_model_kwargs = { "temperature": 0.0 }, 
+                                          prompt = None):
 
     print("Call demo_vectordb_multiquery_retriever")
 
@@ -218,6 +222,42 @@ def demo_vectordb_multiquery_retriever_m2(bedrock_runtime : str,
     )  # "lines" is the key (attribute name) of the parsed output
 
     similar_docs = multiquery_retriever.get_relevant_documents(prompt, kwargs={ "k": 2 })
+
+    print(f"Matches: {len(similar_docs)}")
+    for similar_doc in similar_docs:
+        print("---------------------------------")
+        print(f"{similar_doc.metadata}")
+        print(f"{similar_doc.page_content}")
+
+def demo_vectordb_contextual_retriever(bedrock_runtime : str, 
+                                          embedding_model_id = "amazon.titan-embed-text-v1", 
+                                          llm_model_id = "anthropic.claude-instant-v1", 
+                                          llm_model_kwargs = { "temperature": 0.0 }, 
+                                          prompt = None):
+
+    print("Call demo_vectordb_multiquery_retriever")
+
+    embeddings = BedrockEmbeddings(
+        client = bedrock_runtime,
+        model_id = embedding_model_id
+    )
+
+    llm = BedrockChat(
+        client = bedrock_runtime,
+        model_id = llm_model_id,
+        model_kwargs = llm_model_kwargs,
+    )
+
+    vectordb = Chroma(embedding_function=embeddings, persist_directory=CHROMA_DB_PATH)
+
+    retriever = vectordb.as_retriever()
+
+    compressor = LLMChainExtractor.from_llm(llm)
+
+    compression_retriever = ContextualCompressionRetriever(base_compressor=compressor,
+                                                           base_retriever=retriever)
+
+    similar_docs = compression_retriever.get_relevant_documents(prompt)
 
     print(f"Matches: {len(similar_docs)}")
     for similar_doc in similar_docs:
