d36099d8766f0162aa4499840f38ed782e8bab7a
christoferson <sweetsorrow1981@yahoo.co.jp>
2023-10-31 01:10:19 +0900

    Add demo_access_opensearch

--- demo_bedrock_langchain_vector_store_opensearch.py
+++ demo_bedrock_langchain_vector_store_opensearch.py
@@ -1,3 +1,5 @@
+import config
+
 from langchain.document_loaders import CSVLoader
 from langchain.document_loaders import BSHTMLLoader
 from langchain.document_loaders import PyPDFLoader
@@ -13,6 +15,8 @@ from langchain.embeddings import BedrockEmbeddings
 
 from langchain.vectorstores import Chroma
 
+from opensearchpy import OpenSearch, RequestsHttpConnection
+from requests_aws4auth import AWS4Auth
 
 CHROMA_DB_PATH = "vectordb/chromadb/demo.db"
 
@@ -22,20 +26,66 @@ def run_demo(session):
 
     bedrock_runtime = session.client('bedrock-runtime', region_name="us-east-1")
 
-    model_id = "anthropic.claude-v1"
-    model_id = "anthropic.claude-v2"
-    model_id = "anthropic.claude-instant-v1"
-    model_id = "amazon.titan-embed-text-v1"
+    llm_model_id = "anthropic.claude-instant-v1"
+    embedding_model_id = "amazon.titan-embed-text-v1"
 
+    demo_access_opensearch(session)
     #demo_load_embed_save(bedrock_runtime)
     prompt = "What is the origin of the name New York?"
     prompt = "Etymology New York"
     #demo_vectordb_similarity_search(bedrock_runtime, model_id, prompt)
-    demo_vectordb_retriever(bedrock_runtime, model_id, prompt)
+    #demo_vectordb_retriever(bedrock_runtime, model_id, prompt)
 
+bookinfo_index_settings = {
+  "mappings": {
+    "properties": {
+      "id":{
+        "type": "keyword"
+      },   
+      "bookName":{
+        "type": "text",
+        "analyzer": "kuromoji"
+      },
+      "description":{
+        "type": "text",
+        "analyzer": "kuromoji"
+      },      
+      "publicationDate":{
+        "type": "date",
+      } 
+    }
+  }
+}
 
+def demo_access_opensearch(session):
+    print("Call demo_access_opensearch")
+    client = session.client('opensearchserverless')
+    result = client.list_collections()
+    print(result)
 
-def demo_load_embed_save(bedrock_runtime : str, model_id='amazon.titan-embed-text-v1'):
+    service = 'aoss'
+    region = 'us-east-1'
+    credentials = session.get_credentials()
+    awsauth = AWS4Auth(credentials.access_key, credentials.secret_key,
+                   region, service, session_token=credentials.token)
+    host = config.aws["opensearch_serverless_endpoint"] 
+    client = OpenSearch(
+        hosts=[{'host': host, 'port': 443}],
+        http_auth=awsauth,
+        use_ssl=True,
+        verify_certs=True,
+        connection_class=RequestsHttpConnection,
+        timeout=300
+    )
+
+    index_name = "bookinfo"
+    if not client.indices.exists(index=index_name):
+        client.indices.create(index=index_name, body=bookinfo_index_settings)
+        print("index created")
+    else:
+        print("index already exist")
+
+def demo_load_embed_save(bedrock_runtime : str, embedding_model_id='amazon.titan-embed-text-v1'):
 
     print("Call demo_load_embed_save")
 
@@ -49,7 +99,7 @@ def demo_load_embed_save(bedrock_runtime : str, model_id='amazon.titan-embed-tex
     
     embeddings = BedrockEmbeddings(
         client = bedrock_runtime,
-        model_id = model_id
+        model_id = embedding_model_id
     )
 
     vectordb = Chroma.from_documents(data_chunked, embeddings, persist_directory=CHROMA_DB_PATH)
