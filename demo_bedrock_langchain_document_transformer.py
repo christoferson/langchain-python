1068b876742fd21478594a74fdff28158b6a8d56
christoferson <sweetsorrow1981@yahoo.co.jp>
2023-11-07 20:35:55 +0900

    Add demo_load_txt_and_transform

--- demo_bedrock_langchain_document_transformer.py
+++ demo_bedrock_langchain_document_transformer.py
@@ -7,6 +7,8 @@ from langchain.chat_models import BedrockChat
 from langchain.schema import HumanMessage, AIMessage, SystemMessage
 from langchain.prompts import HumanMessagePromptTemplate, ChatPromptTemplate
 
+from langchain.text_splitter import CharacterTextSplitter
+
 def run_demo(session):
 
     bedrock = session.client('bedrock')
@@ -17,89 +19,24 @@ def run_demo(session):
     model_id = "anthropic.claude-v2"
     model_id = "anthropic.claude-instant-v1"
 
-    model_kwargs = { "temperature": 0.0 }
-    prompt = "What is the diameter of Earth?"
+    demo_load_txt_and_transform(bedrock_runtime)
 
 
-    #demo_load_csv(bedrock_runtime)
-    #demo_load_html(bedrock_runtime)
-    #demo_load_pdf(bedrock_runtime)
-    person_name = "Claude Shannon" #"Tom Cruise"
-    question = "When was he born?"
-    demo_load_wikipedia(bedrock_runtime, model_id, model_kwargs, person_name, question)
 
+def demo_load_txt_and_transform(bedrock_runtime):
 
-def demo_load_csv(bedrock_runtime):
+    print("Call demo_load_txt_and_transform")
 
-    print("Call demo_load_csv")
+    with open("documents/demo.txt", encoding="utf-8") as file:
+        file_text = file.read()
 
-    loader = CSVLoader("documents/demo.csv")
+    #print(file_text)
+    print(len(file_text))
+    print(len(file_text.split()))
 
-    data = loader.load()
+    text_splitter = CharacterTextSplitter(separator="\n\n", chunk_size=1000)
+    
+    texts = text_splitter.create_documents([file_text])
 
-    print(data)
-
-    print(data[0].page_content)
-
-def demo_load_html(bedrock_runtime):
-
-    print("Call demo_load_html")
-
-    loader = BSHTMLLoader("documents/demo.html")
-
-    data = loader.load()
-
-    print(data)
-
-    print(data[0].page_content)
-
-
-def demo_load_pdf(bedrock_runtime):
-
-    print("Call demo_load_pdf")
-
-    loader = PyPDFLoader("documents/demo.pdf")
-
-    data = loader.load()
-
-    print(data)
-
-    #print(data[0].page_content)
-
-
-def demo_load_wikipedia(bedrock_runtime, model_id, model_kwargs, person_name, question):
-
-    print("Call demo_load_wikipedia")
-
-    loader = WikipediaLoader(query=person_name, load_max_docs=1)
-
-    data = loader.load()
-
-    print(data)
-
-    context_text = data[0].page_content
-
-    llm = BedrockChat(
-        client = bedrock_runtime,
-        model_id = model_id,
-        model_kwargs = model_kwargs,
-    )
-
-    template = """Answer this question: 
-    {question}
-    Here is some extra context: 
-    {document}
-    """
-
-    human_prompt = HumanMessagePromptTemplate.from_template(template)
-    chat_prompt = ChatPromptTemplate.from_messages([human_prompt])
-
-    messages = chat_prompt.format_prompt(question=question, document=context_text).to_messages()
-
-    result = llm(messages)
-
-    print("\n\n")
-    print(f"Question: {question}")
-    print("\n")
-    print(f"Answer: {result.content}")
+    print(texts[0])
 
