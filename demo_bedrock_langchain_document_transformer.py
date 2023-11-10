53c21c3e650e96b259e573c75f06a8ab3ecd0313
christoferson <sweetsorrow1981@yahoo.co.jp>
2023-11-10 22:21:59 +0900

    Add transformer

--- demo_bedrock_langchain_document_transformer.py
+++ demo_bedrock_langchain_document_transformer.py
@@ -6,6 +6,7 @@ from langchain.document_loaders import WikipediaLoader
 from langchain.chat_models import BedrockChat
 from langchain.schema import HumanMessage, AIMessage, SystemMessage
 from langchain.prompts import HumanMessagePromptTemplate, ChatPromptTemplate
+from langchain.chains import TransformChain, LLMChain, SimpleSequentialChain
 
 from langchain.text_splitter import CharacterTextSplitter
 
@@ -18,25 +19,42 @@ def run_demo(session):
     model_id = "anthropic.claude-v1"
     model_id = "anthropic.claude-v2"
     model_id = "anthropic.claude-instant-v1"
+    model_kwargs = { "temperature": 0.0, 'max_tokens_to_sample': 200 }
 
-    demo_load_txt_and_transform(bedrock_runtime)
+    demo_load_txt_and_transform(bedrock_runtime, model_kwargs)
+
+def transform_function(inputs : dict) -> dict:
+    text = inputs['text']
+    only_review_text = text.split('REVIEW:')[-1]
+    lower_case_text = only_review_text.lower()
+    return {'output' : lower_case_text}
 
 
-
-def demo_load_txt_and_transform(bedrock_runtime):
+def demo_load_txt_and_transform(bedrock_runtime, model_kwargs):
 
     print("Call demo_load_txt_and_transform")
 
     with open("documents/demo.txt", encoding="utf-8") as file:
         file_text = file.read()
 
-    #print(file_text)
-    print(len(file_text))
-    print(len(file_text.split()))
+    transform_chain = TransformChain(input_variables=['text'], output_variables=['output'], transform=transform_function)
 
-    text_splitter = CharacterTextSplitter(separator="\n\n", chunk_size=1000)
-    
-    texts = text_splitter.create_documents([file_text])
+    template = "Create a 3 sentence summary of the review:\n{review}"
 
-    print(texts[0])
+    llm = BedrockChat(
+        client = bedrock_runtime,
+        model_id = "anthropic.claude-instant-v1",
+        model_kwargs = model_kwargs,
+    )
 
+    prompt = ChatPromptTemplate.from_template(template)
+
+    summary_chain = LLMChain(llm=llm, prompt=prompt, output_key='review_summary')
+
+    sequential_chain = SimpleSequentialChain(chains=[transform_chain, summary_chain], verbose=True)
+
+    result = sequential_chain(file_text)
+
+    #print(result)
+
+    print(result['output'])
