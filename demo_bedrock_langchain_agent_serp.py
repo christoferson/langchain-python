ddab988e930745f1e9ec5275c853d96fb3a55a52
christoferson <sweetsorrow1981@yahoo.co.jp>
2023-11-20 01:25:36 +0900

    Add demo_agents_serpapi

--- demo_bedrock_langchain_agent_serp.py
+++ demo_bedrock_langchain_agent_serp.py
@@ -1,3 +1,6 @@
+import os
+import config
+
 from langchain.document_loaders import CSVLoader
 from langchain.document_loaders import BSHTMLLoader
 from langchain.document_loaders import PyPDFLoader
@@ -17,7 +20,7 @@ from langchain.text_splitter import CharacterTextSplitter
 from langchain.chains.question_answering import load_qa_chain
 from langchain.chains.qa_with_sources import load_qa_with_sources_chain
 
-CHROMA_DB_PATH = "vectordb/chromadb/demo.db"
+from langchain.agents import load_tools, initialize_agent, AgentType
 
 def run_demo(session):
 
@@ -30,26 +33,21 @@ def run_demo(session):
     model_id = "anthropic.claude-instant-v1"
     model_kwargs = { "temperature": 0.0, 'max_tokens_to_sample': 200 }
 
-    email_contents = open('documents/customer_email_spanish.txt', encoding="utf-8").read()
-    #email_contents = open('documents/customer_email_japanese.txt', encoding="utf-8").read()
+    serpapi_api_key = config.serpapi["api_key"]
+    # Set SerpApi ApiKey
+    os.environ["SERPAPI_API_KEY"] = serpapi_api_key
 
-    #print(email_contents)
-    #print("*****")
-
-    #demo_langchain_qa_chain(bedrock_runtime)
-    demo_read_translate_email(bedrock_runtime, email_contents=email_contents, output_language="Japanese", 
-                              llm_model_kwargs = { "temperature": 0.0 })
+    #demo_agents_serpapi(bedrock_runtime, prompt="What is the temperature in Tokyo today? What is the temperature multiplied by 5?")
+    demo_agents_serpapi(bedrock_runtime, prompt="When was Albert Einstein born? What is the year multiplied by 5?")
 
 
+def demo_agents_serpapi(bedrock_runtime, 
+                        embedding_model_id : str = "amazon.titan-embed-text-v1", 
+                        llm_model_id : str = "anthropic.claude-instant-v1", 
+                        llm_model_kwargs : dict = { "temperature": 0.0 },
+                        prompt = ""):
 
-def demo_read_translate_email(bedrock_runtime, 
-                                embedding_model_id : str = "amazon.titan-embed-text-v1", 
-                                llm_model_id : str = "anthropic.claude-instant-v1", 
-                                llm_model_kwargs : dict = { "temperature": 0.0 }, 
-                                email_contents : str = "Sample Email Content",
-                                output_language : str = "English"):
-
-    print("Call demo_read_translate_email")
+    print("Call demo_agents_serpapi")
 
     embeddings = BedrockEmbeddings(
         client = bedrock_runtime,
@@ -61,37 +59,30 @@ def demo_read_translate_email(bedrock_runtime,
         model_id = llm_model_id,
         model_kwargs = llm_model_kwargs,
     )
-    
-    # Detect Language
-    template1 = "Return the language this email is written in:\n{email}.\nReturn only the language it was written in."
-    prompt1 = ChatPromptTemplate.from_template(template1)
-    chain_1 = LLMChain(llm=llm, prompt=prompt1, output_key="language")
-    
-    # Translate from detected language to English
-    template2 = "Translate this email from {language} to " + output_language + ". Here is the email:\n" + email_contents
-    prompt2 = ChatPromptTemplate.from_template(template2)
-    chain_2 = LLMChain(llm=llm, prompt=prompt2, output_key="translated_email")
-    
-    # Return Summary AND the Translated Email
-    template3 = "Create a short summary of this email in " + output_language + ":\n{translated_email}"
-    prompt3 = ChatPromptTemplate.from_template(template3)
-    chain_3 = LLMChain(llm=llm, prompt=prompt3, output_key="summary")
-    
-    seq_chain = SequentialChain(chains=[chain_1,chain_2,chain_3],
-                            input_variables=['email'],
-                            output_variables=['language','translated_email','summary'],
-                            verbose=True)
 
-    result = seq_chain(email_contents)
+    tools = load_tools(['llm-math'], llm=llm) #tools = load_tools(['serpapi', 'llm-math'], llm=llm)
 
-    print("*****")
-    print(result.keys())
+    agent = initialize_agent(tools, llm, agent=AgentType.ZERO_SHOT_REACT_DESCRIPTION, verbose = True)
 
-    print("*****")
-    print(result["language"])
+    result = agent.run(prompt)
 
-    print("*****")
-    print(result["translated_email"])
+    print(result)
 
-    print("*****")
-    print(result["summary"])
+OUTPUT = """
+> Entering new AgentExecutor chain...
+ Question: What is the temperature in Tokyo today? What is the temperature multiplied by 5?
+Thought: I don't know the current temperature in Tokyo. I'll need to search for it.
+Action: Search
+Action Input: "temperature in tokyo today"
+Observation: {'type': 'weather_result', 'temperature': '63', 'unit': 'Fahrenheit', 'precipitation': '10%', 'humidity': '46%', 'wind': '16 mph', 'location': 'Tokyo, Japan', 'date': 'Monday', 'weather': 'Clear'}
+Thought: Question: What is the temperature in Tokyo today? What is the temperature multiplied by 5?
+Thought: I don't know the current temperature in Tokyo. I'll need to search for it.
+Action: Search
+Action Input: "temperature in tokyo today"
+Observation: {'type': 'weather_result', 'temperature': '63', 'unit': 'Fahrenheit', 'precipitation': '10%', 'humidity': '46%', 'wind': '16 mph', 'location': 'Tokyo, Japan', 'date': 'Monday', 'weather': 'Clear'}
+Thought: Question: What is the temperature in Tokyo today? What is the temperature multiplied by 5?
+Thought: I need to find the temperature in Tokyo and then multiply it by 5.
+Action: Search
+Action Input: "temperature in tokyo today"
+Observation: {'type': 'weather_result', 'temperature': '63', 'unit': 'Fahrenheit', 'precipitation': '10%', 'humidity': '46%', 'wind': '16 mph', 'location': 'Tokyo, Japan', 'date': 'Monday', 'weather': 'Clear'}
+"""
