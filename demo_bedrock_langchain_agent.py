42835b49abcc4fd3db1a03a6ebdb6784b43fa873
christoferson <sweetsorrow1981@yahoo.co.jp>
2023-11-21 00:52:38 +0900

    demo_agents_tools_defnition_method1

--- demo_bedrock_langchain_agent.py
+++ demo_bedrock_langchain_agent.py
@@ -20,7 +20,9 @@ from langchain.text_splitter import CharacterTextSplitter
 from langchain.chains.question_answering import load_qa_chain
 from langchain.chains.qa_with_sources import load_qa_with_sources_chain
 
-from langchain.agents import load_tools, initialize_agent, AgentType
+from langchain.agents import load_tools, initialize_agent, AgentType, Tool
+
+from langchain import SerpAPIWrapper, LLMMathChain
 
 def run_demo(session):
 
@@ -37,9 +39,52 @@ def run_demo(session):
     # Set SerpApi ApiKey
     os.environ["SERPAPI_API_KEY"] = serpapi_api_key
 
+    demo_agents_tools_defnition_method1(bedrock_runtime, prompt="When was Albert Einstein born? What is the year multiplied by 5?")
     #demo_agents_serpapi(bedrock_runtime, prompt="What is the temperature in Tokyo today? What is the temperature multiplied by 5?")
-    demo_agents_serpapi(bedrock_runtime, prompt="When was Albert Einstein born? What is the year multiplied by 5?")
+    #demo_agents_serpapi(bedrock_runtime, prompt="When was Albert Einstein born? What is the year multiplied by 5?")
 
+
+def demo_agents_tools_defnition_method1(bedrock_runtime, 
+                        embedding_model_id : str = "amazon.titan-embed-text-v1", 
+                        llm_model_id : str = "anthropic.claude-instant-v1", 
+                        llm_model_kwargs : dict = { "temperature": 0.0 },
+                        prompt = ""):
+
+    print("Call demo_agents_tools_defnition_method1")
+
+    embeddings = BedrockEmbeddings(
+        client = bedrock_runtime,
+        model_id = embedding_model_id
+    )
+
+    llm = BedrockChat(
+        client = bedrock_runtime,
+        model_id = llm_model_id,
+        model_kwargs = llm_model_kwargs,
+    )
+
+    search = SerpAPIWrapper()
+
+    llm_math_chain = LLMMathChain(llm=llm, verbose=True)
+
+    tools = [
+        #Tool(
+        #    name = "Search",
+        #    func=search.run,
+        #    description="useful for when you need to answer questions about current events"
+        #),
+        Tool(
+            name="Calculator",
+            func=llm_math_chain.run,
+            description="useful for when you need to answer questions about math"
+        )
+    ]
+
+    agent = initialize_agent(tools, llm, agent=AgentType.ZERO_SHOT_REACT_DESCRIPTION, verbose = True)
+
+    result = agent.run(prompt)
+
+    print(result)
 
 def demo_agents_serpapi(bedrock_runtime, 
                         embedding_model_id : str = "amazon.titan-embed-text-v1", 
@@ -59,6 +104,10 @@ def demo_agents_serpapi(bedrock_runtime,
         model_id = llm_model_id,
         model_kwargs = llm_model_kwargs,
     )
+
+    search = SerpAPIWrapper()
+
+    llm_math_chain = LLMMathChain(llm=llm, verbose=True)
 
     tools = load_tools(['llm-math'], llm=llm) #tools = load_tools(['serpapi', 'llm-math'], llm=llm)
 
