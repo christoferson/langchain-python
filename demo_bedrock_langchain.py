a2a6d2e6ba686d1412354b48002673764198bdfc
christoferson <sweetsorrow1981@yahoo.co.jp>
2023-11-23 22:05:50 +0900

    Add launch config

--- demo_bedrock_langchain.py
+++ demo_bedrock_langchain.py
@@ -16,14 +16,15 @@ def run_demo(session):
     model_id = "anthropic.claude-instant-v1"
     model_id = "anthropic.claude-v1"
     model_id = "meta.llama2-13b-chat-v1" #NotImplementedError: Provider meta model does not support chat.
+    model_id = "cohere.command-text-v14"
 
     model_kwargs = { "temperature": 0.0 }
     model_kwargs_meta = { "temperature": 0.0, "top_p" : 0.9, "max_gen_len": 512 }
     prompt = "Tell me a fun fact about Pluto."
     prompts = ["Tell me a fun fact about Pluto.", "Tell me a fun fact about Venus."]
 
-    demo_invoke_model_meta(bedrock_runtime, model_id, prompt)
-    #demo_llm(bedrock_runtime, model_id, model_kwargs_meta, prompt)
+    #demo_invoke_model_meta(bedrock_runtime, model_id, prompt)
+    demo_llm(bedrock_runtime, model_id, model_kwargs_meta, prompt)
     #demo_llm_predict(bedrock_runtime, model_id, model_kwargs, prompt)
     #demo_llm_generate(bedrock_runtime, model_id, model_kwargs, prompts)
     #demo_llm_chain(bedrock_runtime, model_id, model_kwargs, prompt)
@@ -31,9 +32,6 @@ def run_demo(session):
 def demo_invoke_model_meta(bedrock_runtime, model_id, prompt):
 
     print(f"Call demo_invoke_model_meta model_id={model_id} prompt={prompt}")
-
-
-    #prompt = f"\n\nHuman: {text}\n\nAssistant:"
 
     request = {
         "prompt": prompt,
