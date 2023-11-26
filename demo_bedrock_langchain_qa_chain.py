0aa677571f84802a9fa2aa5eb81af875782a0b83
christoferson <sweetsorrow1981@yahoo.co.jp>
2023-11-26 13:22:17 +0900

    Add demo_langchain_converse_retrieval_chain_1

--- demo_bedrock_langchain_qa_chain.py
+++ demo_bedrock_langchain_qa_chain.py
@@ -15,7 +15,7 @@ from langchain.prompts import PromptTemplate, HumanMessagePromptTemplate, ChatPr
 from langchain.chains import TransformChain, LLMChain, SimpleSequentialChain
 
 from langchain.text_splitter import CharacterTextSplitter
-from langchain.chains import RetrievalQA, RetrievalQAWithSourcesChain, ConversationalRetrievalChain
+from langchain.chains import RetrievalQA, RetrievalQAWithSourcesChain, ConversationalRetrievalChain, StuffDocumentsChain
 
 from langchain.chains.question_answering import load_qa_chain
 from langchain.chains.qa_with_sources import load_qa_with_sources_chain
@@ -48,7 +48,8 @@ def run_demo(session):
     
     #query = "How did New York get it's name?" # Infinite Loop
     query = "When did New York get it's name?"
-    demo_langchain_retrieval_qa_chain_2(bedrock_runtime, llm_model_id="anthropic.claude-v2", query=query)
+    #demo_langchain_retrieval_qa_chain_2(bedrock_runtime, llm_model_id="anthropic.claude-v2", query=query)
+    demo_langchain_converse_retrieval_chain_1(bedrock_runtime, llm_model_id="anthropic.claude-v2", query=query)
 
 
 
@@ -367,3 +368,98 @@ def demo_langchain_retrieval_qa_chain_3(bedrock_runtime,
 
     result = chat_agent.run(query)
     print(result)
+
+
+def demo_langchain_converse_retrieval_chain_1(bedrock_runtime, 
+                        embedding_model_id : str = "amazon.titan-embed-text-v1", 
+                        llm_model_id : str = "anthropic.claude-instant-v1", 
+                        llm_model_kwargs : dict = { "temperature": 0.0 },
+                        query : str = ""):
+
+    print(f"Call demo_langchain_retrieval_qa_chain llm_model_id={llm_model_id} ")
+
+    # 1. Create Prompt Template for Claude
+
+    template_qg = """
+    次の会話に対しフォローアップの質問があるので、フォローアップの質問を独立した質問に言い換えなさい。
+            
+    チャットの履歴:
+    {chat_history}
+            
+    フォローアップの質問:
+    {question}
+            
+    言い換えられた独立した質問:"""
+
+    prompt_qg = PromptTemplate(
+            template=template_qg,
+            input_variables=["chat_history", "question"],
+            output_parser=None,
+            partial_variables={},
+            template_format='f-string',
+            validate_template=True,
+    )
+#####
+    prompt_template_qa = """You are a helpful assistant. If the context is not relevant, please answer the question by using your own knowledge about the topic. 
+
+    {context}
+
+    Question: {question}
+    Answer:"""
+
+    prompt_qa = PromptTemplate(
+            template=prompt_template_qa, 
+            input_variables=["context", "question"]
+    )
+    chain_type_kwargs = {"prompt": prompt_qa}
+
+    # 2. Instantiate Claude Bedrock
+
+    embeddings = BedrockEmbeddings(
+        client = bedrock_runtime,
+        model_id = embedding_model_id
+    )
+
+    llm = BedrockChat(
+        client = bedrock_runtime,
+        model_id = llm_model_id,
+        model_kwargs = llm_model_kwargs,
+    )
+
+    # 3. Create RetrievalQA
+
+    #combine_docs_chain = StuffDocumentsChain()
+    #chain_type_kwargs = {"prompt": PROMPT}
+    vectordb = Chroma(embedding_function=embeddings, persist_directory=CHROMA_DB_PATH)
+    retriever = vectordb.as_retriever(search_type="similarity", search_kwargs={"k": 3}) #search_kwargs.k defines number of ducuments to search
+
+    # Keep track of questions & Answers
+    memory = ConversationBufferMemory(ai_prefix="Assistant")
+    
+    
+    # 4. Invoke
+
+    question_generator = LLMChain(llm=llm, prompt=prompt_qg)
+    doc_chain = load_qa_chain(llm=llm, chain_type="stuff", prompt=prompt_qa)
+
+    qa = ConversationalRetrievalChain(
+            retriever=retriever,
+            question_generator=question_generator,
+            combine_docs_chain=doc_chain,
+    )
+
+    ##
+    questions = [
+        "What is the origin of the name New York?",
+        "When was the city New York Established?",
+    ]
+    chat_history = []
+    ###
+    result = qa.run({"question": query, "chat_history": chat_history})
+    print(f"Q: {query}")
+    print(f"A: {result}")
+    #for question in questions:
+    #    print(f"Q: {question}")
+    #    result = qa.run({"question": question, "chat_history": chat_history})
+    #    print(f"A: {result}")
+    #    chat_history.append((question, result))
